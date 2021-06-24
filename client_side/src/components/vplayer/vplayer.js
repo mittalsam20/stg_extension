f8e087a160c99a6114c9ba93a07df366afc576ce
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-06-24 20:27:37 +0530

    react player finally done

--- client_side/src/components/vplayer/vplayer.js
+++ client_side/src/components/vplayer/vplayer.js
@@ -1,8 +1,34 @@
-import ReactPlayer from "react-player";
+import ReactPlayer from "react-player/lazy";
 import axios from "axios";
 import { useState, useEffect } from "react";
+// import AppBar from "@material-ui/core";
+// import ToolBar from "@material-ui/core";
+// import { makeStyles } from "@material-ui/core";
+import { Typography, Grid, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  playerWrapper: {
+    position: "relative",
+  },
+  controlsWrapper: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    background: "rgba(0,0,0,0.6)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    zIndex: 1,
+  },
+});
 const Vplayer = () => {
-  const [currentUrl, setcurrentUrl] = useState("");
+  const classes = useStyles();
+  const [currentUrl, setcurrentUrl] = useState(
+    "http://localhost:5000/recording/recording_1624542675237.webm"
+  );
+  const [fastLoad, setfastLoad] = useState(true);
   const retUrl = async () => {
     const res = await axios.get("http://localhost:5000/app/getrecurl");
     const data = await res.data;
@@ -17,8 +43,30 @@ const Vplayer = () => {
   // }, [currentUrl]);
   return (
     <div>
-      <div className="player-wrapper">
-        <ReactPlayer controls width="100%" height="100%" url={currentUrl} />
+      <div className={classes.playerWrapper}>
+        <ReactPlayer
+          controls
+          width="100%"
+          height="100%"
+          light={fastLoad}
+          url={currentUrl}
+          onReady={() => {
+            console.log("onready");
+            setfastLoad(false);
+          }}
+        />
+        <div className={classes.controlsWrapper}></div>
+        <Grid
+          container
+          direction="row"
+          alignItems="center"
+          justify="space-between"
+          style={{ padding: 16 }}
+        >
+          <Grid item>
+            <Typography variant="h5"></Typography>
+          </Grid>
+        </Grid>
       </div>
       <button
         onClick={() => {
