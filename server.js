741dac3fc7533b6c13beecbb98089fa80e023672
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-06-29 16:57:49 +0530

    syntax

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.use("/app", noteRoute);
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client_side/build"))
-    ap.get("*", (req, res) => {
+    app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client_side", "build", "index.html"));
     })
 }
